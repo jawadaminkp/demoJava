ab363f734362070353212d08885cf4346c0d9c9f
javadaminkp <javadaminkp@192.168.0.114>
2021-01-21 16:59:34 +0500

    I have added some code to my project

---
+++ demoJava/src/demojava/DemoJava.java
@@ -0,0 +1,24 @@
+package demojava;
+
+
+public class DemoJava {
+
+   
+    public static void main(String[] args) {
+       
+        System.out.println("We are doing this demo");
+        
+        int[] list=new int[5];
+        
+        
+        list[0]=35;
+        list[1]=3;
+        list[2]=2;
+        list[3]=34;
+        list[4]=65;
+        
+        System.out.println(list[0]);
+        
+    }
+    
+}
